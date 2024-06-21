f50cca998e02278799e78be3de33fbcac7565488
Daniel <165353614+danturiano@users.noreply.github.com>
2024-06-21 22:57:54 +0800

    answer exercises 1-9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+  let newString = inputString.replace(/\W/g, "");
+  let newArr = [];
+  for (let i = 0; i <= newString.length; i++) {
+    newArr[i] = newString[newString.length - i];
+  }
+  let palind = newArr.join("").toLowerCase();
+  let check = newString.toLowerCase();
+  console.log(check);
+  if (check == palind) {
+    return true;
+  } else return false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
